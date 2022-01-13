832d66bfee643a2c3b623d94375f3b11cd29e329
JongCHONG <yonhoa27@hotmail.fr>
2022-01-13 12:37:50 +0100

    userroutes

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require("express")
+const morgan = require("morgan")
 const app = express()
 const port = 5000
 const { dbConnect } = require("./config/db")
 
+const userRoutes = require("./routes/user")
+
 dbConnect()
 
 app.use(express.json())
+app.use(morgan("tiny"))
+
+app.use("./user", userRoutes)
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
