d80ea7ba0659a1b0197bc95e5051847f5f6f83e4
becca <rebecca.kaci@gmail.com>
2022-01-17 15:05:57 +0100

    passeport.js terminé

---
+++ config/passport.js
@@ -0,0 +1,27 @@
+const passport = require("passport")
+const passportLocal = require("passport-local")
+const User = require("../models/User")
+
+const LocalStrategy = passportLocal.Strategy
+
+passport.use(new LocalStrategy( async (username, password, done) => {
+    const user = await user.findOne({ username: username, password: password }).exec()
+    
+    if (!user){
+        return done (null, false)
+    } 
+
+    return done(null, user)
+}))
+
+passport.serializeUser((_id, done) => {
+    done(null, User._id)
+})
+
+passport.deserializeUser((id, done) => {
+    const user = await User.findById(id).exec()
+
+    done(null, user)
+})
+
+module.exports = passport
