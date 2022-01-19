36e8f168773f4315fbfceb39fe45922892aaf370
becca <rebecca.kaci@gmail.com>
2022-01-19 15:39:01 +0100

    ajout de l'id d'un user via son Schema dans le model Tweet

--- models/Tweet.js
+++ models/Tweet.js
@@ -1,18 +1,24 @@
 const { Schema, model } = require("mongoose")
 
 const TweetSchema = Schema({
-  user: {
-		type: String,
-		// required: true
-	},
   text: {
 		type: String,
-		// required: true
+		maxlength : 280
 	},
   pinnedTweet: Boolean,
   likes: Number,
-  retweets: [{ type: Schema.Types.ObjectId, ref: "User" }],
-  comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }]
+  user: {
+    type: Schema.Types.ObjectId, 
+    ref:"User" 
+  },
+  comments: [{ 
+    type: Schema.Types.ObjectId, 
+    ref: "Comment" 
+  }],
+  retweets: [{ 
+    type: Schema.Types.ObjectId, 
+    ref: "User" 
+  }]
 },{
   timestamps: true
 })
